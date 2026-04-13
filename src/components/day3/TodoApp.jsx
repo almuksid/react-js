1c9d7ed120713377da65b2feee2f9fecd1c45a29
Al Muksid <deveelopment.almuksid@gmail.com>
2026-04-13 13:59:16 +0600

    Todo Project Completed

--- src/components/day3/TodoApp.jsx
+++ src/components/day3/TodoApp.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import { useState } from 'react'
+
+import './Todo.css'
 const TodoApp = () => {
+  const [filter, setFilter] = useState("all")
   const [todoTitle, setTodoTitle] = useState("")
   const [todoList, setTodoList] = useState([
     {id:1, title:"todo1", isCompleted:true},
@@ -33,20 +36,33 @@ const TodoApp = () => {
   }
   return (
     <div>
-      <h2>Todo App</h2>
+      <div className="mb-4 bgCol p-3">
+        <h2 style={{color:'white', textAlign:'center', marginBottom:'30px'}}>Todo App</h2>
 
-      <form action="" onSubmit={submitHandler}>
-        <input type="text" value={todoTitle} onChange={event => setTodoTitle(event.target.value)} />
-        <button type="submit">Create Todo</button>
-      </form>
+        <button class="btn btn-success filterBtn" type="submit" onClick={() => setFilter("all")}>All</button>
+        <button class="btn btn-secondary filterBtn" type="submit" onClick={() => setFilter("active")}>Active</button>
+        <button class="btn btn-secondary filterBtn" type="submit" onClick={() => setFilter("completed")}>Completed</button><br /><br />
 
-      <ul>
-        {todoList.map(todo => <li key={todo.id}>
-          <input type="checkbox"  checked={todo.isCompleted} onChange={ () => checkedHandler(todo.id)}/>
-          <span>{todo.title}</span>
-          <button type="submit" onClick={() => removeHandler(todo.id)}>Remove Todo</button>
-        </li>)}
-      </ul>
+
+        <form class="input-group mb-3" action="" onSubmit={submitHandler}>
+          <input class="form-control" type="text" value={todoTitle} onChange={event => setTodoTitle(event.target.value)} />
+          <button style={{float:'right'}} class="btn btn-success" type="submit">Create Todo</button>
+        </form><br />
+
+        <ul class="list-group">
+          {todoList
+          .filter(todo => {
+            if (filter === "all") return true
+            if (filter === "active") return !todo.isCompleted
+            if (filter === "completed") return todo.isCompleted
+          })
+          .map(todo => <li class="list-group-item" key={todo.id}>
+            <input type="checkbox"  checked={todo.isCompleted} onChange={ () => checkedHandler(todo.id)}/>
+            <span>{todo.title}</span>
+            <button style={{float:'right'}} class="btn btn-danger" type="submit" onClick={() => removeHandler(todo.id)}>Remove Todo</button>
+          </li>)}
+        </ul>
+      </div>
     </div>
   )
 }
